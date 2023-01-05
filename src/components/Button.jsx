971bf91a218fbab14ad4b09c3ffcec32b6a8a99b
shopifykingbd <shopifykingbd@gmail.com>
2023-01-05 19:11:03 +0600

    testing

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins mt-8 font-medium text-[18px] text-primary outline-none rounded-[10px]`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
